dec866bd50388fa72e3f4ddbc7166132e2f4d5c9
danilotiago <programacao.desenvolvimento@gmail.com>
2019-03-25 15:49:49 -0300

    implementando logica de adicao de digitos

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -5,7 +5,17 @@ import './Calculator.css'
 import Button from '../components/Button/Button'
 import Display from '../components/display/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
+
+    state = {...initialState}
 
     constructor(props) {
         super(props)
@@ -17,7 +27,7 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        console.log('Limpando memória')
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
@@ -25,13 +35,46 @@ export default class Calculator extends Component {
     }
 
     addDigit(digit) {
-        console.log(digit)
+
+        // regra para evitar ter 2 pontos na calculadora
+        if (digit === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        // regra para trocar limpar o display se só tiver o digito zero
+        // ou se clearDisplay do estado for true
+        const clearDisplay = this.state.displayValue === "0"
+                            || this.state.clearDisplay
+
+        // valor atual sera vazio se o display tiver de ser limpo
+        // ou pega o valor do state
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+
+        // o novo valor sera o valor atual concatenado com o novo digito
+        const displayValue = currentValue + digit
+
+        // atualiza o estado do componente com os novos parametros
+        // caso for adicionado um novo digito, nao deve resetar o display
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (digit !== '.') {
+            // pega o indice atual do array de valores
+            const i = this.state.current
+            // pega e converte o novo valor atual
+            const newValue = parseFloat(displayValue)
+            // pega o array de valores do state.values
+            const values = [...this.state.values]
+            // atribui o novo valor convertido para a posicao i
+            values[i] = newValue
+            // atualiza o estado
+            this.setState({values})
+        }
     }
 
     render() {
         return (
             <div className="calculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple />
                 <Button label="/" click={this.setOperation} operation />
                 <Button label="7" click={this.addDigit} />
